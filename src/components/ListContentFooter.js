71b01f288131afba8daa6dc912e51de5a4ed783b
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-13 19:10:08 +0300

    redux

--- src/components/ListContentFooter.js
+++ src/components/ListContentFooter.js
@@ -5,7 +5,7 @@ function ListContentFooter() {
   return (
     <div className="content-footer">
           <div className="left-item">3 kaldı.</div>
-          <div className="menu-item">Tümü</div>
+          <div className="active menu-item">Tümü</div>
           <div className="menu-item">Tamamlanan</div>
     </div>
   );
