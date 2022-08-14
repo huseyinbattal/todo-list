5333064d500da8f46fbf70722f0ba6646ca4980a
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-14 10:27:14 +0300

    action

--- src/store/actionTypes/todoActionsTypes.js
+++ src/store/actionTypes/todoActionsTypes.js
@@ -1,1 +1,4 @@
 export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const COMPLETE_TODO = "COMPLETE_TODO";
+export const SET_TODO_FILTER = "SET_TODO_FILTER";
---
+++ src/store/actions/todoActions.js
@@ -0,0 +1,9 @@
+import { ADD_TODO, COMPLETE_TODO, DELETE_TODO, SET_TODO_FILTER } from "../actionTypes/todoActionsTypes";
+
+
+export const addTodo = () => {
+    return {
+        type: ADD_TODO,
+        payload:
+    }
+}
