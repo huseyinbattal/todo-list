fc3b74af07447b1114122b90dbada55d2bd755ed
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-15 17:28:38 +0300

    Update todoReducer.js

--- src/store/reducers/todoReducer.js
+++ src/store/reducers/todoReducer.js
@@ -1,15 +1,17 @@
-import { ADD_TODO } from "../actionTypes/todoActionsTypes";
+import { ADD_TODO, DELETE_TODO } from "../actionTypes/todoActionsTypes";
 import { todoState } from "../state/todoState";
 
 function todoReducer(state = todoState, action) {
-    switch (action.type) {
-        case ADD_TODO:
-            
-            break;
-    
-        default:
-            break;
-    }
+  switch (action.type) {
+    case ADD_TODO:
+      return { ...state, todoList: [...state.todoList, action.payload] };
+
+    case DELETE_TODO:
+      return { ...state, todoList: state.todoList.filter() };
+
+    default:
+      break;
+  }
 }
 
 export default todoReducer();
