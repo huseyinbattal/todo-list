2f41fb3c7057d8f4596fb1c2af67fd3c291f3c2c
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-15 16:15:21 +0300

    Update todoReducer.js

--- src/store/reducers/todoReducer.js
+++ src/store/reducers/todoReducer.js
@@ -1,6 +1,15 @@
-
+import { ADD_TODO } from "../actionTypes/todoActionsTypes";
 import { todoState } from "../state/todoState";
 
 function todoReducer(state = todoState, action) {
+    switch (action.type) {
+        case ADD_TODO:
+            
+            break;
     
+        default:
+            break;
+    }
 }
+
+export default todoReducer();
