fd5fde5d5fbbce439669f83124cd65a5eff98a06
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-16 13:10:26 +0300

    Update InputToDo.js

--- src/components/InputToDo.js
+++ src/components/InputToDo.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
 function InputToDo() {
+  const [text, setText] = useState("");
   return (
     <div className="input-wrapper">
-      <input placeholder="Create New ToDo" autoFocus />
+      <input
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
+        placeholder="Create New ToDo" autoFocus />
     </div>
   );
 }
