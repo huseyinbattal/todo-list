4e99a920614905c444dfefbd5f32aff34dff8086
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-27 14:41:04 +0300

    Update ListContentFooter.js

--- src/components/ListContentFooter.js
+++ src/components/ListContentFooter.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setTodoFilter } from "../store/actions/todoActions";
 import "./ListContentFooter.css"
 
 function ListContentFooter() {
+  const { todoFilter, todoList } = useSelector((state) => state.todo)
+  const dispatch=useDispatch()
   return (
     <div className="content-footer">
-          <div className="left-item">3 kaldı.</div>
-          <div className="active menu-item">Tümü</div>
-          <div className="menu-item">Tamamlanan</div>
+      <div className="left-item">
+        {todoList.filter((x)=>x.complete).length}
+        kaldı.</div>
+      <div className={`menu-item ${todoFilter === "all"}`} onClick={() => {
+        dispatch(setTodoFilter("all"))
+          }}>Tümü</div>
+          <div  onClick={() => {
+        dispatch(setTodoFilter("completed"))
+          }} className={`menu-item ${todoFilter==="complete"}`}>Tamamlanan</div>
     </div>
   );
 }
